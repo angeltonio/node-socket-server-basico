c0fa5e4e291dd3e12f59224fd330d5dfc284f7eb
alexfdz55 <alexander.fernandez5495@gmai;.com>
2023-01-05 13:27:30 -0500

    cambios vercel.json 8

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const Server = require('./models/server');
-
-
-const server = new Server();
+// const Server = require('./models/server');
 
 
+// const server = new Server();
 
-server.listen();
 
+
+// server.listen();
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
 
