b20bb26860bef1072dc94353b7273185770971db
alexfdz55 <alexander.fernandez5495@gmai;.com>
2023-01-06 17:41:52 -0500

    webskocket mediante un endpoint 7

--- app.js
+++ app.js
@@ -12,40 +12,42 @@
 
 
 
-var express = require('express');
-var app = express();
-var expressWs = require('express-ws')(app);
-const path = require('path');
-const cors = require('cors');
+// var express = require('express');
+// var app = express();
+// var expressWs = require('express-ws')(app);
+// const path = require('path');
+// const cors = require('cors');
 
-app.use( cors() );
-app.use(express.static('public'))
+// app.use( cors() );
+// app.use(express.static('public'))
 
-app.use(function (req, res, next) {
-    console.log('middleware');
-    req.testing = 'testing';
-    return next();
-});
+// app.use(function (req, res, next) {
+//     console.log('middleware');
+//     req.testing = 'testing';
+//     return next();
+// });
 
-app.get('/', function (req, res, next) {
-    console.log('get route', req.testing);
-    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
-    res.end();
-});
+// app.get('/', function (req, res, next) {
+//     console.log('get route', req.testing);
+//     res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+//     res.end();
+// });
 
-app.ws('/', function (ws, req) {
-    ws.on('message', function (msg) {
-        console.log(msg);
-    });
-    console.log('socket', req.testing);
-});
+// app.ws('/', function (ws, req) {
+//     ws.on('message', function (msg) {
+//         console.log(msg);
+//     });
+//     console.log('socket', req.testing);
+// });
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor corriendo en puerto', port);
-});
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log('Servidor corriendo en puerto', port);
+// });
 
+
+//--------------------------------------------------------------
 
 // const express = require('express');
 // const app = express();
@@ -60,3 +62,28 @@ app.listen(process.env.PORT || 3000, () => {
 // app.listen(process.env.PORT || 3000);
 
 // module.exports = app;
+
+//----------------------------------------------------
+
+
+const express = require('express')
+const enableWs = require('express-ws')
+
+const app = express()
+enableWs(app)
+
+app.ws('/echo', (ws, req) => {
+    ws.on('message', msg => {
+        ws.send(msg)
+    })
+
+    // ws.on('close', () => {
+    //     console.log('WebSocket was closed')
+    // })
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en puerto', port);
+});
