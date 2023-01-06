972d473b5ef1f26b3837fbf6dba8674b9e8ec7fd
alexfdz55 <alexander.fernandez5495@gmai;.com>
2023-01-06 17:08:29 -0500

    webskocket mediante un endpoint

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const Server = require('./src/models/server');
-
-
-const server = new Server();
+// const Server = require('./src/models/server');
 
 
-server.listen();
+// const server = new Server();
+
+
+// server.listen();
+
+
+
+
+
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+
+app.use(function (req, res, next) {
+    console.log('middleware');
+    req.testing = 'testing';
+    return next();
+});
+
+app.get('/', function (req, res, next) {
+    console.log('get route', req.testing);
+    res.end();
+});
+
+app.ws('/msg', function (ws, req) {
+    ws.on('message', function (msg) {
+        console.log(msg);
+    });
+    console.log('socket', req.testing);
+});
+
+app.listen(3000);
 
 
 // const express = require('express');
 // const app = express();
 // const path = require('path');
 
-// app.use(express.static('public'))
+// // app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
-//     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-// })
+// // app.get('/', (req, res) => {
+// //     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+// // })
 
 // app.listen(process.env.PORT || 3000);
 
 // module.exports = app;
-
