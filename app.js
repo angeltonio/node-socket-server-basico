42115cfeaca442aa8ebb1c075510a22462b0c36a
alexfdz55 <alexander.fernandez5495@gmai;.com>
2023-01-18 17:00:49 -0500

    controlando esp32 desde la pagina web

--- app.js
+++ app.js
@@ -15,16 +15,40 @@ const wss = new Server({ server });
 wss.on('connection', (ws) => {
     console.log('Client connected');
     ws.on('close', () => console.log('Client disconnected'));
-    ws.on('message', (msg) => {
-        console.log(msg.BYTES_PER_ELEMENTS);
-    })
+
+    ws.on('message', function message(data) {
+        // const msg = data.toString();
+
+        // const msg = JSON.parse(data.toString());
+
+        console.log('received: %s', data);
+
+        wss.clients.forEach(client => {
+            client.send(data.toString());
+        })
+        // console.log(msg)
+    });
 });
 
 
+// let on_off = true;
 
+// setInterval(() => {
+//     wss.clients.forEach((client) => {
+//         // client.send(new Date().toTimeString());
 
-setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
-    });
-}, 1000);
+//         on_off = !on_off;
+
+//         const data = {
+//             on_off: on_off,
+//             power: true,
+//             temp: true,
+//             timer: true,
+//             mas: true,
+//             menos: true
+//         };
+
+//         client.send(JSON.stringify(data));
+//     });
+
+// }, 1000);
