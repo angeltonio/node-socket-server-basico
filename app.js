57615eedec7abf0037626da832f8a309d2e4c59a
alexfdz55 <alexander.fernandez5495@gmai;.com>
2023-01-31 17:04:13 -0500

    mejoras (detectar la conxion y desconexion del esp)

--- app.js
+++ app.js
@@ -1,20 +1,66 @@
 'use strict';
 
-const express = require('express');
-const { Server } = require('ws');
+// const express = require('express');
+// const { Server, WebSocket } = require('ws');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
 
-const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// const server = express()
+//     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new Server({ server });
+// const wss = new Server({ server });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
+// wss.on('connection', (ws, req) => {
+//     // ws.id = wss.getUniqueID();
+//     const ip = req.socket.remoteAddress;
+//     console.log('Client connected with ip: ', ip);
+
+//     let clientsQuantity = 0;
+//     wss.clients.forEach(client => {
+//         if (client.OPEN === 1) {
+//             clientsQuantity++;
+//         }
+//         // console.log('Client.ID: ' + client.id)
+//     });
+//     console.log('Cantidad de clientes: ', clientsQuantity);
+
+//     ws.on('close', () => {
+//         clientsQuantity = 0;
+//         console.log('Client disconnected:', ip)
+//     });
+
+//     ws.on('message', function message(data, isBinary) {
+//         console.log('received: %s', data);
+//         wss.clients.forEach(function each(client) {
+//             if (client.readyState === WebSocket.OPEN) {
+//                 client.send(data, { binary: isBinary });
+//             }
+//         });
+//     });
+// });
+
+
+const { WebSocketServer } = require('ws');
+
+function heartbeat() {
+    this.isAlive = true;
+}
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws, req) {
+    const ip = req.socket.remoteAddress;
+    console.log('Client connected with ip: ', ip);
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
+
+
+    ws.on('close', function close() {
+        // clearInterval(interval);
+        console.log('Client disconnected:', ip);
+    });
 
     ws.on('message', function message(data) {
         // const msg = data.toString();
@@ -26,10 +72,43 @@ wss.on('connection', (ws) => {
         wss.clients.forEach(client => {
             client.send(data.toString());
         })
-        // console.log(msg)
+
     });
 });
 
+
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) {
+            console.log('se desconecto: ');
+            return ws.terminate();
+        }
+
+        ws.isAlive = false;
+        ws.ping();
+    });
+}, 2000);
+
+
+
+    // ws.on('message', function message(data) {
+    //     // const msg = data.toString();
+
+    //     // const msg = JSON.parse(data.toString());
+
+    //     // console.log('received: %s', data);
+
+    //     // wss.clients.forEach(client => {
+    //     //     client.send(data.toString());
+    //     // })
+
+
+    //     wss.clients.forEach(function each(client) {
+    //         if (client.readyState === WebSocket.OPEN) {
+    //             client.send(data, { binary: isBinary });
+    //         }
+    //     });
+    // });
 
 // let on_off = true;
 
